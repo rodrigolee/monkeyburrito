e1baa2ae3141f67d360488ecaa6cada22bbfc387
Jonathan Cho <jonofcho@gmail.com>
2017-05-29 18:34:51 -0700

    create custom user signupform @ /userprofilesignup

---
+++ codemaster/userprofile/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'userprofile'
+urlpatterns = [
+    url(r'^signup/$', views.signup, name="signup"),
+]
--- codemaster/userprofile/views.py
+++ codemaster/userprofile/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login , authenticate
+from userprofile.forms import SignUpForm
 # Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+
+    return render(request, 'userprofile/signup.html', {'form': form})
